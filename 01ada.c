0cebc70a76e0c8f8e0e2176b4019f1daee6daf1b
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2024-04-18 05:12:48 +0000

    lab 01

---
+++ 01ada.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+void qsort(int[], int, int);
+int  partition(int[], int, int);
+void qsort(int a[], int first, int last){
+    int j;
+    if(first<last){
+        j= partition(a, first, last+1);
+        qsort(a, first, j-1);
+        qsort(a, j+1, last);
+    }
+}
+
+int partition(int a[], int first, int last){
+    int v = a[first];
+    int i = first;
+    int j = last;
+    int temp =0;
+    do{
+        do{
+            i++;
+        }
+        while(a[j]>v);
+        if(i<j){
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }while(i<j);
+    a[first] = a[j];
+    a[j] = v;
+    return j;
+}
+
+int main(){
+    int a[40], i, n;
+    printf("\nEnter the no of elements (size): ");
+    scanf("%d", &n);
+    printf("\nEnter the Elements to sort : ");
+    for(i=0; i<n; i++){
+        printf("%d", i);
+        scanf("%d", &a[i]);
+    }
+    qsort(a,0, n-1);
+    printf("\nThe Elements after sorting are : ");
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+
+    return 0;
+    
+}
