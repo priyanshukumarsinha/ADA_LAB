9728f843ca824c6759b8d0d177fa0a765f49cdb4
Sneha R <136419281+SnehaR17@users.noreply.github.com>
2024-04-18 05:32:01 +0000

    lab2 done half

--- 02lab.c
+++ 02lab.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+#include<conio.h>
+void merge(int[],int,int,int);
+void merge sort(int[],int,int);
+void merge(int a[25],int low, int m)
+{
+    int b[25],h,i,j,k;
+    h=low;
+    i=low;
+    j=mid+1;
+    while(h<=mid)&&(j<=high))
+    {
+        if(a[h],a[j])
+        {
+            
+        }
+    }
+}
